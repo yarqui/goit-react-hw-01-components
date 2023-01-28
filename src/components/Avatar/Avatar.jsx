32b77324a7c14261c0f0d0a493a4bd4841e587a7
yarqui <108986486+yarqui@users.noreply.github.com>
2023-01-28 12:39:54 +0200

    added some components

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,6 @@
+import user from 'user.json';
+import { IMG_SIZE } from 'config/config.js';
+
+export const Avatar = ({ imgURL, alt }) => (
+  <img src={imgURL} alt={alt} width={IMG_SIZE}></img>
+);
